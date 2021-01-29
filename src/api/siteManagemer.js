65bf1a887e58ef7f26d6e9ac0de67e27c4de3fee
H.zhang.hao <zhang.hao@redstarclouds.com>
2021-01-29 20:53:20 +0800

    init

---
+++ src/api/siteManagemer.js
@@ -0,0 +1,8 @@
+import Http from '../utils/axios'
+const rt = 'http://139.198.188.175:8090/'
+export default {
+    //计划申报
+    getPlanTitle() {
+        return Http.get(`${rt}/device/plan/infoType`)
+    },
+}
--- src/router/siteManagemer.js
+++ src/router/siteManagemer.js
@@ -19,6 +19,102 @@ export default {
    meta: [{name: '位置显示', parentName: '场地维护', path: '/declare-in-stock'}],
    component: () => import('@/views/siteManagemer/locationDisplay/index.vue')
   }, 
+  {
+   path: 'location-display',
+   name: 'field-maintain-location-display',
+   meta: [{name: '训练基地', parentName: '场地维护', path: '/declare-in-stock'}],
+   component: () => import('@/views/siteManagemer/locationDisplay/index.vue')
+  }, 
+  {
+   path: 'location-display',
+   name: 'field-maintain-location-display',
+   meta: [{name: '训练场地', parentName: '场地维护', path: '/declare-in-stock'}],
+   component: () => import('@/views/siteManagemer/locationDisplay/index.vue')
+  }, 
+  {
+   path: 'location-display',
+   name: 'field-maintain-location-display',
+   meta: [{name: '保障用房', parentName: '场地维护', path: '/declare-in-stock'}],
+   component: () => import('@/views/siteManagemer/locationDisplay/index.vue')
+  }, 
+  {
+   path: 'location-display',
+   name: 'field-maintain-location-display',
+   meta: [{name: '配置设施', parentName: '场地维护', path: '/declare-in-stock'}],
+   component: () => import('@/views/siteManagemer/locationDisplay/index.vue')
+  }, 
+  {
+   path: 'location-display',
+   name: 'field-maintain-location-display',
+   meta: [{name: '阵地工事', parentName: '场地维护', path: '/declare-in-stock'}],
+   component: () => import('@/views/siteManagemer/locationDisplay/index.vue')
+  }, 
+  {
+   path: 'location-display',
+   name: 'field-maintain-location-display',
+   meta: [{name: '道路管网', parentName: '场地维护', path: '/declare-in-stock'}],
+   component: () => import('@/views/siteManagemer/locationDisplay/index.vue')
+  }, 
+  {
+   path: 'location-display',
+   name: 'field-maintain-location-display',
+   meta: [{name: '军官训练中心', parentName: '场地维护', path: '/declare-in-stock'}],
+   component: () => import('@/views/siteManagemer/locationDisplay/index.vue')
+  }, 
+  {
+   path: 'location-display',
+   name: 'field-maintain-location-display',
+   meta: [{name: '位置显示', parentName: '场地维护', path: '/declare-in-stock'}],
+   component: () => import('@/views/siteManagemer/locationDisplay/index.vue')
+  }, 
+  {
+   path: 'location-display',
+   name: 'field-maintain-location-display',
+   meta: [{name: '教导机构', parentName: '场地维护', path: '/declare-in-stock'}],
+   component: () => import('@/views/siteManagemer/locationDisplay/index.vue')
+  }, 
+  {
+   path: 'location-display',
+   name: 'field-maintain-location-display',
+   meta: [{name: '普通教室', parentName: '场地维护', path: '/declare-in-stock'}],
+   component: () => import('@/views/siteManagemer/locationDisplay/index.vue')
+  }, 
+  {
+   path: 'location-display',
+   name: 'field-maintain-location-display',
+   meta: [{name: '专业训练中心', parentName: '场地维护', path: '/declare-in-stock'}],
+   component: () => import('@/views/siteManagemer/locationDisplay/index.vue')
+  }, 
+  {
+   path: 'location-display',
+   name: 'field-maintain-location-display',
+   meta: [{name: '图书馆', parentName: '场地维护', path: '/declare-in-stock'}],
+   component: () => import('@/views/siteManagemer/locationDisplay/index.vue')
+  }, 
+  {
+   path: 'location-display',
+   name: 'field-maintain-location-display',
+   meta: [{name: '军体馆', parentName: '场地维护', path: '/declare-in-stock'}],
+   component: () => import('@/views/siteManagemer/locationDisplay/index.vue')
+  }, 
+  {
+   path: 'location-display',
+   name: 'field-maintain-location-display',
+   meta: [{name: '演播室(电教设备)', parentName: '场地维护', path: '/declare-in-stock'}],
+   component: () => import('@/views/siteManagemer/locationDisplay/index.vue')
+  }, 
+  {
+   path: 'location-display',
+   name: 'field-maintain-location-display',
+   meta: [{name: '实验室管理', parentName: '场地维护', path: '/declare-in-stock'}],
+   component: () => import('@/views/siteManagemer/locationDisplay/index.vue')
+  }, 
+  {
+   path: 'location-display',
+   name: 'field-maintain-location-display',
+   meta: [{name: '维护管理', parentName: '场地维护', path: '/declare-in-stock'}],
+   component: () => import('@/views/siteManagemer/locationDisplay/index.vue')
+  }, 
  ]
 }
 
