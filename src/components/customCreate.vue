2080b9808d9c2b4c1bd37427f5d7afc6dc7a060e
zuiqingfeng <H>
2021-02-23 15:28:50 +0800

    init

--- src/components/customCreate.vue
+++ src/components/customCreate.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="customSearch">
     <el-form
+      ref="forms"
       class="search-from"
       label-position="right"
       label-width="150px"
@@ -14,39 +15,56 @@
           v-for="item in searchList"
           :key="item.name"
         >
-          <el-form-item :label="item.comment" v-if="item.editable && (item.type == 'string' || item.type == 'date' || item.type == 'datetime' || item.type == 'int' || item.type == 'map' || item.type == 'list')">
+          <el-form-item
+            :label="item.comment"
+            :prop="item.name"
+             :rules="{
+                required: true,
+                message: `请输入${item.comment}`,
+                trigger: 'blur',
+              }"
+            v-if="
+              item.editable &&
+              (item.type == 'string' ||
+                item.type == 'int')
+            "
+          >
             <el-input
               v-if="item.type == 'string'"
               size="small"
               v-model="form[item.name]"
               placeholder="请输入"
-            ></el-input>
-
-            <el-date-picker
-              style="width: 100%"
-              v-if="item.type == 'date' || item.type == 'datetime'"
-              v-model="form[item.name]"
-              size="small"
-              type="date"
-              placeholder="选择日期"
-              value-format="yyyy-MM-dd"
             >
-            </el-date-picker>
-
+              ></el-input
+            >
             <el-input
               v-if="item.type == 'int'"
               type="number"
               size="small"
               v-model.number="form[item.name]"
               placeholder="请输入"
-            ></el-input>
-
+             
+            >
+              ></el-input
+            >
+          </el-form-item>
+            <el-form-item
+            :label="item.comment"
+            :prop="item.name"
+              :rules="{
+                required: true,
+                message: `请选择${item.comment}`,
+                trigger: 'change',
+              }"
+            v-if="item.editable &&item.type == 'map' "
+          >
             <el-select
               placeholder="请选择"
               v-model="form[item.name]"
               size="small"
               v-if="item.type == 'map'"
             >
+              >
               <el-option
                 v-for="(list, index) in setSearchList(item)"
                 :key="index"
@@ -70,11 +88,51 @@
               >
               </el-option>
             </el-select>
+          </el-form-item>
+            <el-form-item
+            :label="item.comment"
+            :prop="item.name"
+            v-if="
+              item.editable &&
+                item.type == 'list'
+            "
+          >
 
-
-
-            
-
+            <el-select
+              placeholder="请选择"
+              v-model="form[item.name]"
+              size="small"
+              v-if="item.type == 'list'"
+            >
+              <el-option
+                v-for="(list, index) in setSearchList2(item)"
+                :key="index"
+                :label="list"
+                :value="list"
+              >
+              </el-option>
+            </el-select>
+          </el-form-item>
+             <el-form-item
+            :label="item.comment"
+            :prop="item.name"
+            v-if="
+              item.editable &&
+              (
+                item.type == 'date' ||
+                item.type == 'datetime')
+            "
+          >
+            <el-date-picker
+              style="width: 100%"
+              v-if="item.type == 'date' || item.type == 'datetime'"
+              v-model="form[item.name]"
+              size="small"
+              type="date"
+              placeholder="选择日期"
+              value-format="yyyy-MM-dd"
+            >
+            </el-date-picker>
           </el-form-item>
         </el-col>
       </el-row>
@@ -124,15 +182,21 @@ export default {
       let list = [];
       this.searchList.forEach((item) => {
         if (item.name == event.name) {
-          if(item.itemdata && item.itemdata.length>0){
-            list = item.itemdata.split(',')
+          if (item.itemdata && item.itemdata.length > 0) {
+            list = item.itemdata.split(",");
           }
         }
       });
       return list;
     },
     onSumit() {
-      this.$emit("listCreate", this.form);
+      this.$refs["forms"].validate((valid) => {
+        if (valid) {
+          this.$emit("listCreate", this.form);
+        } else {
+          return false;
+        }
+      });
     },
     onSearch() {
       console.log(this.checkedSearchList);
--- src/components/customEdit.vue
+++ src/components/customEdit.vue
@@ -7,94 +7,133 @@
       :model="form"
     >
       <el-row :gutter="10" style="margin-top: 20px">
-        <template v-for="item in searchList">
           <el-col
-            :md="8"
-            :sm="12"
-            :xs="24"
-            :key="item.name"
-            v-if="item.editable && (item.type != 'attachment' || item.type == 'string' || item.type == 'date' || item.type == 'datetime' || item.type == 'int' || item.type == 'map' || item.type == 'list')"
+          :md="8"
+          :sm="12"
+          :xs="24"
+          v-for="item in searchList"
+          :key="item.name"
+        >
+          <el-form-item
+            :label="item.comment"
+            :prop="item.name"
+             :rules="{
+                required: true,
+                message: `请输入${item.comment}`,
+                trigger: 'blur',
+              }"
+            v-if="
+              item.editable &&
+              (item.type == 'string' ||
+                item.type == 'int')
+            "
           >
-            <el-form-item :label="item.comment">
-              <el-input
-                v-if="item.type == 'string'"
-                size="small"
-                v-model="form[item.name]"
-                placeholder="请输入"
-              ></el-input>
-
-              <el-date-picker
-                style="width: 100%"
-                v-if="item.type == 'datetime'"
-                v-model="form[item.name]"
-                type="date"
-                size="small"
-                placeholder="选择日期"
-                format="yyyy-MM-dd"
-                value-format="yyyy-MM-dd"
-                :clearable="false"
-              >
-              </el-date-picker>
-
-              <el-input
-                v-if="item.type == 'int'"
-                type="number"
-                size="small"
-                v-model.number="form[item.name]"
-                placeholder="请输入"
-              ></el-input>
-
-              <el-select
-                placeholder="请选择"
-                v-model.lazy="form[item.name]"
-                size="small"
-                v-if="item.type == 'map' || item.type == 'list'"
-              >
-                <el-option
-                  v-for="(list, index) in setSearchList(item)"
-                  :key="index"
-                  :label="list.name"
-                  :value="list.id"
-                >
-                </el-option>
-              </el-select>
-
-              <el-select
-                  placeholder="请选择"
-                  v-model="form[item.name]"
-                  size="small"
-                  v-if="item.type == 'list'"
-              >
-                <el-option
-                    v-for="(list, index) in setSearchList2(item)"
-                    :key="index"
-                    :label="list"
-                    :value="list"
-                >
-                </el-option>
-              </el-select>
-            </el-form-item>
-          </el-col>
-          <!-- <el-col
-            :md="24"
-            :sm="24"
-            :xs="24"
-            v-if="item.editable && item.type == 'attachment'"
+            <el-input
+              v-if="item.type == 'string'"
+              size="small"
+              v-model="form[item.name]"
+              placeholder="请输入"
+            >
+              ></el-input
+            >
+            <el-input
+              v-if="item.type == 'int'"
+              type="number"
+              size="small"
+              v-model.number="form[item.name]"
+              placeholder="请输入"
+             
+            >
+              ></el-input
+            >
+          </el-form-item>
+            <el-form-item
+            :label="item.comment"
+            :prop="item.name"
+              :rules="{
+                required: true,
+                message: `请选择${item.comment}`,
+                trigger: 'change',
+              }"
+            v-if="item.editable &&item.type == 'map' "
           >
-            <el-form-item :label="item.comment">
-              <el-upload
-                class="upload-demo"
-                :action='url'
-                :on-remove="handleRemove"
-                :on-progress='onProgress(item.name)'
-                :on-success='handleSuccessful'
-                :file-list='setAttachmentList(form[item.name], item.name)'
+            <el-select
+              placeholder="请选择"
+              v-model="form[item.name]"
+              size="small"
+              v-if="item.type == 'map'"
+            >
               >
-                <el-button size="small" type="primary">点击上传</el-button>
-              </el-upload>
-            </el-form-item>
-          </el-col> -->
-        </template>
+              <el-option
+                v-for="(list, index) in setSearchList(item)"
+                :key="index"
+                :label="list.name"
+                :value="list.id"
+              >
+              </el-option>
+            </el-select>
+
+            <el-select
+              placeholder="请选择"
+              v-model="form[item.name]"
+              size="small"
+              v-if="item.type == 'list'"
+            >
+              <el-option
+                v-for="(list, index) in setSearchList2(item)"
+                :key="index"
+                :label="list"
+                :value="list"
+              >
+              </el-option>
+            </el-select>
+          </el-form-item>
+            <el-form-item
+            :label="item.comment"
+            :prop="item.name"
+            v-if="
+              item.editable &&
+                item.type == 'list'
+            "
+          >
+
+            <el-select
+              placeholder="请选择"
+              v-model="form[item.name]"
+              size="small"
+              v-if="item.type == 'list'"
+            >
+              <el-option
+                v-for="(list, index) in setSearchList2(item)"
+                :key="index"
+                :label="list"
+                :value="list"
+              >
+              </el-option>
+            </el-select>
+          </el-form-item>
+             <el-form-item
+            :label="item.comment"
+            :prop="item.name"
+            v-if="
+              item.editable &&
+              (
+                item.type == 'date' ||
+                item.type == 'datetime')
+            "
+          >
+            <el-date-picker
+              style="width: 100%"
+              v-if="item.type == 'date' || item.type == 'datetime'"
+              v-model="form[item.name]"
+              size="small"
+              type="date"
+              placeholder="选择日期"
+              value-format="yyyy-MM-dd"
+            >
+            </el-date-picker>
+          </el-form-item>
+        </el-col>
       </el-row>
       <div class="search-button-block" style="margin-top: 20px">
         <el-button size="small" @click="onSumit()">保 存</el-button>
@@ -114,51 +153,51 @@ export default {
     },
     form: {
       type: Object,
-      default: function() {
-        return {}
-      }
+      default: function () {
+        return {};
+      },
     },
     url: {
       type: String,
-      default: ''
-    }
+      default: "",
+    },
   },
   data() {
     return {
       // fileList: [],
-      queryName: ''
+      queryName: "",
     };
   },
   methods: {
     onProgress(event) {
       this.queryName = event;
     },
-     handleSuccessful(res, file, fileList) {
-      if (res.code == '0000') {
-
-         console.log(res);
-         fileList[fileList.length-1].code = this.queryName;
-          console.log(fileList);
-          console.log(this.form[this.queryName])
-          let query =JSON.parse(this.form[this.queryName]).files.push(file.name);
-          console.log(query)
+    handleSuccessful(res, file, fileList) {
+      if (res.code == "0000") {
+        console.log(res);
+        fileList[fileList.length - 1].code = this.queryName;
+        console.log(fileList);
+        console.log(this.form[this.queryName]);
+        let query = JSON.parse(this.form[this.queryName]).files.push(file.name);
+        console.log(query);
         this.$message.success("上传成功");
       } else {
         // console.log(res)
-        this.$message.error(res.msg|| '系统异常');
+        this.$message.error(res.msg || "系统异常");
       }
     },
     onCancel() {
       this.$emit("close");
     },
     handleRemove(file) {
-      if(!file) {
-        return
+      if (!file) {
+        return;
       }
-      let query =JSON.parse(this.form[file.code])
-       query.files = query.files.filter(item  =>{
-     return item != file.name});
-     this.form[file.code] = JSON.stringify(query);
+      let query = JSON.parse(this.form[file.code]);
+      query.files = query.files.filter((item) => {
+        return item != file.name;
+      });
+      this.form[file.code] = JSON.stringify(query);
     },
     setAttachmentList(list, name) {
       if (list) {
@@ -168,24 +207,23 @@ export default {
           let query = {
             name: item,
             url: item,
-            code: name
+            code: name,
           };
           fileList.push(query);
         });
-        console.log(fileList)
+        console.log(fileList);
         return fileList;
         // return JSON.parse(list).files;
       }
       return [];
     },
     setSearchList(event) {
-
       let list = [];
       this.searchList.forEach((item) => {
         if (item.name == event.name) {
-           if(!item.itemdata) {
-        return
-      }
+          if (!item.itemdata) {
+            return;
+          }
           Object.entries(JSON.parse(item.itemdata)).forEach((item) => {
             let query = {
               id: Number(item[0]),
@@ -201,15 +239,21 @@ export default {
       let list = [];
       this.searchList.forEach((item) => {
         if (item.name == event.name) {
-          if(item.itemdata && item.itemdata.length>0){
-            list = item.itemdata.split(',')
+          if (item.itemdata && item.itemdata.length > 0) {
+            list = item.itemdata.split(",");
           }
         }
       });
       return list;
     },
     onSumit() {
-      this.$emit("listEdit", this.form);
+        this.$refs["forms"].validate((valid) => {
+        if (valid) {
+          this.$emit("listCreate", this.form);
+        } else {
+          return false;
+        }
+      });
     },
   },
 };
